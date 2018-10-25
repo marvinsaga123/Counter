cf54836a4d1b574676606e381d163b9ca53905c9
Marvin Sagastume <msagastume123@gmail.com>
2018-10-24 22:13:41 -0400

    Created application

--- Counter WatchKit Extension/InterfaceController.swift
+++ Counter WatchKit Extension/InterfaceController.swift
@@ -8,22 +8,51 @@
 
 import WatchKit
 import Foundation
-// 2 - Import Watch Connectivity framework
 
+// 2 - Import Watch Connectivity framework
+import WatchConnectivity
 
 // 3 - Add WCSessionDelegate to class definition
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
      // 1 - Create a variable for your counter
-
+    var counter = 0
+    @IBOutlet var labelCount: WKInterfaceLabel!
     
      // 4 - Set up Watch Connectivity
+    private let session : WCSession? =
+        WCSession.isSupported() ?
+        WCSession.default : nil
     
+    override init() {
+        super.init()
+        
+        session?.delegate = self
+        session?.activate()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {}
     
      // 5 - Setup your incrementCounter() function
-    
+    @IBAction func incrementCount() {
+        counter += 1
+        labelCount.setText(String(counter))
+    }
     
      // 6 - Setup your saveCounter() function
-
+    @IBAction func saveCount() {
+        let applicationData = ["counterValue" : counter]
+        
+        if let session = session, session.isReachable {
+            session.sendMessage(applicationData,
+                                replyHandler: nil,
+                                errorHandler: { error in print(error)})
+        }
+    }
+    
+    @IBAction func resetCount() {
+        counter = 0
+        labelCount.setText(String(counter))
+    }
 }
